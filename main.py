e291682c467ba24057f37410ed759caedf3d0625
Shutt3rr <bryanmora8114@gmail.com>
2025-03-27 08:44:28 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+print("Hello World")
+
+words = [["ghtin", "night"], ["lapemyhbs",  "blasphemy"],["rete", "tree"], ["adir", "raid"], ["ofelrw", "flower"], ["eblta", "table"], ["okbgoba", "bookbag"], ["iatgmnaniio", "imagination"], ["rgosugeo", "gorgeous"], ["xraenrytiaodr", "extraordinary"]]
+
+
+
+
